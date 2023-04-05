670370085a94a6b6aa881c23b4fd156bae30447c
notdeltah <deltahalt@gmail.com>
2023-04-05 21:58:45 +0100

    Add support for work creating. Still incomplete

--- src/data/library_controller.py
+++ src/data/library_controller.py
@@ -7,8 +7,22 @@ load_dotenv()
 
 gh = github.Github(os.getenv("GH_API_TOKEN"))
 
+
 def library_repo() -> github.Repository.Repository:
     return gh.get_repo("Writers-Cave/data")
 
+
 def get_library() -> dict:
     return json.loads(library_repo().get_contents("library/library.json").decoded_content)
+
+
+def create_work(title: str, authors: list[str], genres: list[str], url: str):
+    library = get_library()
+    tick = library["data"]["id_ticker"]
+    library["data"]["ticker"] += 1
+
+    library["library"][str(tick)] = work
+
+    library_file = library_repo().get_contents("library/library.json")
+    library_repo().update_file(library_file.path,
+                               f"Added {work['title']}", library, library_file.sha)
