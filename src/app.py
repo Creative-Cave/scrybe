2b3f3f727ba1ed13fd837f9474cb1391ab93074d
D3 <118836504+notdeltah@users.noreply.github.com>
2023-04-10 16:00:52 +0000

    Added pickup line generator

--- src/app.py
+++ src/app.py
@@ -7,27 +7,28 @@ from threading import Thread
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-	return ("Bot is Connected to Discord")
+  return ("TAKE A LOOK AT MY CANVA DESIGN 🗣️")
+
 
 def site():
-	app.run(
-		host="0.0.0.0", 
-		port=random.randint(2000, 9000)
-	)
+  app.run(host="0.0.0.0", port=random.randint(2000, 9000))
+
 
 def stay_alive():
-	while True:
-		start = time.time()
-		
-		while True:
-			end = time.time()
+  while True:
+    start = time.time()
 
-			# This 15 is for the amount of minutes you change it and take it upto 30 minutes at most
-			if ((end - start) >= (15 * 60)):
-				urllib.requests.urlopen("https://scrybe.writerscave.repl.co")
-				break
+    while True:
+      end = time.time()
+
+      # This 15 is for the amount of minutes you change it and take it upto 30 minutes at most
+      if ((end - start) >= (15 * 60)):
+        urllib.request.urlopen("https://scrybe.writerscave.repl.co")
+        break
+
 
 site_thread = Thread(target=site)
 ping_thread = Thread(target=stay_alive)
---
+++ src/cogs/stats.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+
+class ServerStats(commands.Cog):
+
+  def __init__(self, bot):
+    self.bot = bot
+
+  @commands.slash_command(guild_ids=[915996676144111706])
+  async def members(self, ctx):
+    response = await ctx.send_response("Counting members...")
+
+    members = await ctx.guild.fetch_members(limit=None).flatten()
+    bots = len([m for m in members if m.bot])
+    humans = len(members) - bots
+
+    await response.edit_original_response(content = f"Humans - {humans}\nBots - {bots}\nTotal - {len(members)}")
+
+
+def setup(bot):
+  bot.add_cog(ServerStats(bot))
