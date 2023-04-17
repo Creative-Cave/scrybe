92acb6f122370c117926c07ee355fb326a935998
notdeltah <deltahalt@gmail.com>
2023-04-17 16:40:21 +0100

    Use multiple application keys

--- src/cogs/chatbot_rewrite.py
+++ src/cogs/chatbot_rewrite.py
@@ -1,8 +1,12 @@
 import discord
 import requests
 import json
+import random
+import os
+from dotenv import load_dotenv
 from discord.ext import commands
 
+load_dotenv()
 
 class Chatbot(commands.Cog):
 
@@ -11,15 +15,16 @@ class Chatbot(commands.Cog):
 
     @commands.slash_command(guild_ids=[915996676144111706])
     @discord.commands.default_permissions(administrator=True)
+    @discord.commands.option("message", description="The prompt that the bot will use to generate a response", max_length=1024)
     async def ask_scrybe(self, ctx, message: str):
         response = await ctx.send_response("Scrybe is thinking...")
 
-        embed = discord.Embed(title="Ask Scrybe", colour=discord.Colour.teal())
+        embed = discord.Embed(title="Ask Scrybe", colour=discord.Colour.blue())
         embed.add_field(name="You said:", value=message, inline=False)
 
         data = {
-            "application": "8583352252014867612",
-            "instance": "46045911",
+            "application": random.choice(os.getenv("BL_IDS").split("_")),
+            "instance": "47118083",
             "message": message
         }
 
@@ -32,7 +37,7 @@ class Chatbot(commands.Cog):
 
         embed.add_field(name="Scrybe says:", value=bot_message, inline=False)
 
-        await response.edit_original_response(content="", embed=embed)
+        await response.edit_original_response(content="", embed=embed.set_footer(text=f"ID: {data['application']}"))
 
 
 def setup(bot):
